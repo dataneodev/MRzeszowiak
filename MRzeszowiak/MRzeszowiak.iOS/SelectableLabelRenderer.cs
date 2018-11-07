eb96d3107a09d3300562f38ad8885df7a4d321a0
dataneo <BPrv6QazVbwni@gmail.com>
2018-11-07 20:37:59 +0100

    SelectableLabel add

---
+++ MRzeszowiak/MRzeszowiak.iOS/SelectableLabelRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using MRzeszowiak.Extends;
+using MRzeszowiak.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SelectableLabel), typeof(SelectableLabelRenderer))]
+namespace MRzeszowiak.iOS
+{
+    public class SelectableLabelRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null) return;
+
+            Control.Selectable = true;
+            Control.Editable = false;
+            Control.ScrollEnabled = false;
+            Control.TextContainerInset = UIEdgeInsets.Zero;
+            Control.TextContainer.LineFragmentPadding = 0;
+        }
+    }
+}
---
+++ MRzeszowiak/MRzeszowiak/Extends/SelectableLabel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MRzeszowiak.Extends
+{
+    public class SelectableLabel : Editor
+    {
+        public SelectableLabel()
+        {
+        }
+    }
+}
